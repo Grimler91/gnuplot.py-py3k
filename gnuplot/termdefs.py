09387f9e14ca0f06f637604e990c0d008fc0bc69
Pavel Trutman <pavel.trutman@gmail.com>
2017-04-07 20:51:28 +0200

    Added enhanced|noenhanced option for the pdf terminal.

--- gnuplot/termdefs.py
+++ gnuplot/termdefs.py
@@ -392,6 +392,14 @@ terminal_opts['pdf'] = [
         options=['landscape', 'portrait', 'eps', 'default'],
         argname='mode',
         ),
+    KeywordOrBooleanArg(
+        options=['enhanced', 'noenhanced'],
+        # This default should probably be computed from the *current*
+        # value of GnuplotOpts, not at import time. ###
+        default=(gp.GnuplotOpts.prefer_enhanced_postscript
+                 and 'enhanced'
+                 or 'noenhanced'),
+        ),
     KeywordOrBooleanArg(options=['color', 'monochrome']),
     KeywordOrBooleanArg(options=['solid', 'dashed']),
     KeywordOrBooleanArg(
