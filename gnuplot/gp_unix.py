b03c10b714bcdc44238072f25d8d8ac3b0d5e99d
Henrik Grimler <henrik@grimler.se>
2020-12-03 17:04:29 +0100

    gp_unix: fix AttributeError: module 'string' has no attribute 'find'
    Only happens if gnuplot is not installed. With this the persist test
    works and the plotting instead fails with a
    
       gnuplot: command not found
    
    error, which is easier to interpret.

--- gnuplot/gp_unix.py
+++ gnuplot/gp_unix.py
@@ -126,12 +126,11 @@ def test_persist():
     """
 
     if GnuplotOpts.recognizes_persist is None:
-        import string
         g = popen('echo | %s -persist 2>&1' % GnuplotOpts.gnuplot_command, 'r')
         response = g.readlines()
         g.close()
         GnuplotOpts.recognizes_persist = (
-            (not response) or (string.find(response[0], '-persist') == -1))
+            (not response) or (response[0].find('-persist') == -1))
     return GnuplotOpts.recognizes_persist
 
 
